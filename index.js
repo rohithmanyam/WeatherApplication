07109377af425c4025256113a34c4e3017f53c94
rohithmanyam <manyamrohith@gmail.com>
2020-08-18 02:10:51 +0100

    first commit

--- index.js
+++ index.js
@@ -1,21 +1,79 @@
-$(document).ready(function() {
-	var url = 'http://hn.algolia.com/api/v1/search?query=javascript';
-	$.getJSON(url, function(data) {
-		var currentQuote = '';
-		var quotes = data.hits;
-		$('.read').on('click', function(){
-			currentQuote = quotes[Math.floor(Math.random() * quotes.length)]
-			console.log(currentQuote);
-			$('.quoteBody').hide();
-			$('.quoteBodyLink').html(currentQuote.title);
-			$('.quoteBodyLink').attr('href', currentQuote.url).attr('target', '_blank');
-			$('.quoteAuthor').html(currentQuote.author);
-			$('.tweetQuote').attr('href', 'https://twitter.com/intent/tweet?text= ' + currentQuote.title + '-' +  currentQuote.author )
-			.attr('target', '_blank')
-			.attr('disabled', false);
-			$('.read').html('show me more');
-		});
-		// console.log(quotes[0].title);
-		// console.log(data);
-	});
+$(document).ready(function(){
+$('.short').hide();
+if(navigator.geolocation){
+	var currentPosition = '';
+	navigator.geolocation.getCurrentPosition(function(position){
+		currentPosition = position;
+		var latitude = currentPosition.coords.latitude;
+		var longitude = currentPosition.coords.longitude;
+		console.log(latitude, longitude);
+		var url = 'http://api.weatherstack.com/current?access_key=476603384959095deb1397d7abc0ffa7&query=';
+		$.getJSON(url + latitude + ',' + longitude, function(data){
+			var data = JSON.stringify(data);
+			var json = JSON.parse(data);
+			var country = json.location.country;
+			var city = json.location.name;
+			var state = json.location.region;
+
+			var temp =json.current.temperature;
+			var location = json.location.localtime.replace('-', '');
+			 var wind = json.current.wind_speed;
+			 var humidity = json.current.humidity;
+			 var time = json.location.localtime;
+			 var cloud = json.current.cloudcover;
+
+
+			 //console.log(data);
+
+			$('#weather').html(city + ',' + state + ',' + country);
+			if(temp < 18){
+				$('.grey-jumbo').css({
+					backgroundImage: 'url(https://cdn.pixabay.com/photo/2018/11/19/11/29/heart-3825101_1280.jpg)'
+				});
+				$('#temp').html("<h1>It's a pretty cold day today!!<hr></h1>");
+			} else if(temp > 10 && tem <28) {
+				$('.grey-jumbo').css({
+					backgroundImage: 'url(https://cdn.pixabay.com/photo/2018/05/14/14/09/clouds-3400094_1280.jpg)'
+				});
+				$('#temp').html("<h1>It's a sunny day today!!<hr></h1>");	
+			} else {
+				$('.grey-jumbo').css({
+					backgroundImage: 'url(https://cdn.pixabay.com/photo/2016/03/27/19/09/summer-1283730_1280.jpg)'
+				});
+				$('#temp').html("<h1>It's a hot day today!!<hr></h1>");
+			}
+			//toggle temperature;
+			$('#info1').html(time + 'hrs');
+			$('#info2').html('Wind ' + wind + 'kph');
+			$('#info3').html(temp + '&#8451');
+
+			$('.short').show();
+			
+				var yes = true;
+				$('#switch').on('click', function(){
+				if (yes) {
+					$('#info3').html(temp+32 + '&#8457');
+					$('#switch').html('show in celsius');
+					yes = false;
+
+				} else{
+					$('#info3').html(temp + '&#8451' );
+					$('#switch').html('show in farenheight');
+					yes = true; 
+				}
+			});
+				//showing sky status
+				if(cloud <= 30){
+					$('#info5').html('Clear Sky');
+				} else {
+					$('#info5').html('Cloudy Sky');
+				}
+				$('#info6').html('Humidity ' + humidity + '%');
+
+		} );
+		//console.log(currentPosition);
+	})
+}
 });
+
+//http://api.weatherstack.com/current?access_key=476603384959095deb1397d7abc0ffa7&query=
